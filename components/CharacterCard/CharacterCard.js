424702fac0c27b38f66ed18b46395ae35cf6ea9f
Stephanie Preuthen <kann.zaubern@gmail.com>
2025-01-13 12:12:39 +0100

    create and render character cards

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,1 +1,69 @@
-export function createCharacterCard() {}
+import { renderCards } from "../../utils.js";
+
+const cardContainer = document.querySelector(".card-container");
+
+export function createCharacterCard(character) {
+  let card = document.createElement("li");
+  card.classList.add("card");
+  let cardImageContainer = document.createElement("div");
+  cardImageContainer.classList.add("card__image-container");
+
+  card.append(cardImageContainer);
+
+  let cardImage = document.createElement("img");
+  cardImage.classList.add("card__image");
+  let cardImageGradient = document.createElement("div");
+  cardImageGradient.classList.add("card__image-gradient");
+
+  cardImageContainer.append(cardImage, cardImageGradient);
+  let cardContent = document.createElement("div");
+  cardContent.classList.add("card__content");
+
+  card.append(cardContent);
+
+  let cardTitle = document.createElement("h2");
+  cardTitle.classList.add("card__title");
+  let cardInfo = document.createElement("dl");
+  cardInfo.classList.add("card__info");
+
+  cardContent.append(cardTitle, cardInfo);
+
+  let cardStatusTitle = document.createElement("dt");
+  cardStatusTitle.classList.add("card__info-title");
+  let cardStatusContent = document.createElement("dd");
+  cardStatusContent.classList.add("card__info-description");
+
+  let cardTypeTitle = document.createElement("dt");
+  cardTypeTitle.classList.add("card__info-title");
+  let cardTypeContent = document.createElement("dd");
+  cardTypeContent.classList.add("card__info-description");
+
+  let cardOccurenceTitle = document.createElement("dt");
+  cardOccurenceTitle.classList.add("card__info-title");
+  let cardOccurenceContent = document.createElement("dd");
+  cardOccurenceContent.classList.add("card__info-description");
+
+  cardInfo.append(
+    cardStatusTitle,
+    cardStatusContent,
+    cardTypeTitle,
+    cardTypeContent,
+    cardOccurenceTitle,
+    cardOccurenceContent
+  );
+
+  cardContainer.append(card);
+
+  // Filling the card with data
+  cardImage.setAttribute("src", character.image);
+  cardTitle.innerText = character.name;
+  cardStatusTitle.innerText = "Status";
+  cardStatusContent.innerText = character.status;
+  cardTypeTitle.innerText = "Type";
+  cardTypeContent.innerText = character.type;
+  cardOccurenceTitle.innerText = "Occurence";
+
+  cardOccurenceContent.innerText = character.episode.length;
+
+  renderCards(cardContainer);
+}
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/CharacterCard/CharacterCard.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -8,7 +10,27 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
+const url = "https://rickandmortyapi.com/api/character";
+
 // States
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+fetchCharacters();
+
+// Fetch Characters from API
+
+async function fetchCharacters() {
+  const response = await fetch(url); // HTTP request
+  const data = await response.json(); // Fetching and storing required data – the json method returns the data from that request
+  console.log("Response:", response);
+  console.log("Data:", data);
+
+  console.log("tests", data.results[0].episode.length);
+
+  // Creating and rendering cards
+  data.results.forEach((character) => {
+    createCharacterCard(character);
+  });
+}
