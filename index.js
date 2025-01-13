9118b4570a31fcceb637177f545d8bc7ebd66b0f
Stephanie Preuthen <kann.zaubern@gmail.com>
2025-01-13 17:08:44 +0100

    bug fixes

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 import { createCharacterCard } from "./components/CharacterCard/CharacterCard.js";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector(
-  '[data-js="search-bar-container"]'
-);
 const searchBar = document.querySelector('[data-js="search-bar"]');
-const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
@@ -13,7 +9,7 @@ const searchInput = document.querySelector(".search-bar__input");
 
 let url = "https://rickandmortyapi.com/api/character";
 
-// States
+// Intializing States
 let maxPage = 1;
 let page = 1;
 let searchQuery = "";
@@ -27,37 +23,45 @@ fetchCharacters(url);
 searchBar.addEventListener("submit", (e) => {
   e.preventDefault();
   searchQuery = searchInput.value;
+  page = 1;
 
-  if (searchQuery) {
-    url += `/?name=${searchQuery}`;
-  }
+  let searchUrl = url + `/?name=${searchQuery}`;
+  console.log("Search URL:", searchUrl);
 
   cardContainer.replaceChildren();
-  // render search
-  fetchCharacters(url);
+  // Render search results
+  pagination.innerText = `${page} / ${maxPage}`;
+  fetchCharacters(searchUrl);
   searchBar.reset();
 });
 
 nextButton.addEventListener("click", () => {
-  page += 1;
-  let paginationUrl = url + `?page=${page}`;
-  if (searchQuery != "") {
-    paginationUrl = url + `&page=${page}`;
+  if (page == maxPage) {
+    return false;
   }
+  page += 1;
+  let paginationUrl = url + `?page=${page}`; // Assuming search query is empty; we don't just want the base URL, we want to filter
+  if (searchQuery != "") {
+    paginationUrl = url + `/?name=${searchQuery}` + `&page=${page}`; // Assuming it's not empty (i.e. user searched for Rick), we only add the page parameter on top
+  }
+  console.log("Pagination URL:", paginationUrl);
   cardContainer.replaceChildren();
+  pagination.innerText = `${page} / ${maxPage}`;
   fetchCharacters(paginationUrl);
 });
 
 prevButton.addEventListener("click", () => {
   if (page == 1) {
-    return false;
+    return false; // Everything below in this function won't be considered
   }
   page -= 1;
   let paginationUrl = url + `?page=${page}`;
   if (searchQuery != "") {
-    paginationUrl = url + `&page=${page}`;
+    paginationUrl = url + `/?name=${searchQuery}` + `&page=${page}`;
   }
+  console.log("Pagination URL:", paginationUrl);
   cardContainer.replaceChildren();
+  pagination.innerText = `${page} / ${maxPage}`;
   fetchCharacters(paginationUrl);
 });
 
@@ -65,12 +69,14 @@ prevButton.addEventListener("click", () => {
 async function fetchCharacters(apiUrl) {
   const response = await fetch(apiUrl); // HTTP request
   const data = await response.json(); // Fetching and storing required data – the json method returns the data from that request
-  console.log(apiUrl);
+
   // verify if data is existent
   if (data) {
+    console.log("Data:", data);
+    maxPage = data.info.pages;
+    pagination.innerText = `${page} / ${maxPage}`;
+
     data.results.forEach((character) => {
-      maxPage = data.info.pages;
-      pagination.innerText = `${page} / ${maxPage}`;
       createCharacterCard(character);
     });
   }
